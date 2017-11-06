aae7c494659adcc1a3034e9dd5cb8b9578a68c1b
krishan507 <krishan507@users.noreply.github.com>
2017-11-06 12:23:27 -0600

    Initial commit

---
+++ projectTest.m
@@ -0,0 +1,33 @@
+
+
+img_dir = './'; % folder of the stack images
+N=32; % number of the images in the DATA folder
+
+strfile = 'IM_0001';
+    img = dicomread(fullfile(img_dir, strfile));
+    siz_img = size(img);
+    iradonMatrix=zeros (siz_img(1),siz_img(2),N);
+
+   while(true)
+   for ii=1:N
+        strfile = sprintf('IM_%04d',ii);
+        image=dicomread(fullfile(img_dir, strfile));
+        infoImage = dicominfo(fullfile(img_dir, strfile));
+        R = radon(image,0:179);
+        I2 = iradon(R,0:179,'linear','Shepp-Logan');
+        iradonMatrix (:,:,1,ii) = image;
+        imshow(imcomplement(I2), [])
+   end
+   for ii=N:-1:1
+        strfile = sprintf('IM_%04d',ii);
+        image=dicomread(fullfile(img_dir, strfile));
+        infoImage = dicominfo(fullfile(img_dir, strfile));
+        R = radon(image,0:179);
+        I2 = iradon(R,0:179,'linear','Shepp-Logan');
+        iradonMatrix (:,:,1,ii) = image;
+        imshow(imcomplement(I2), [])
+
+   end
+   end
+
+  
